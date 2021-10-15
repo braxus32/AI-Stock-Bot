c989678e81424d522685ca9c500fd2bb88e03844
andrewbauer2021 <92130710+andrewbauer2021@users.noreply.github.com>
2021-10-14 17:15:49 -0700

    Add files via upload
    Processes and splits the data into two segments for training and testing.

---
+++ Lab3.py
@@ -0,0 +1,82 @@
+"""
+ Name: Your Name
+ Assignment: Lab 3 - Process dataset
+ Course: CS 330
+ Semester: Fall 2021
+ Instructor: Dr. Cao
+ Date: the current date
+ Sources consulted: any books, individuals, etc consulted
+ Known Bugs: description of known bugs and other program imperfections
+ Creativity: anything extra that you added to the lab
+ Instructions: instructions to user on how to execute your program
+
+"""
+import sys
+import argparse
+import math
+
+def splitData(data, trainData, testData, ratio):
+    """
+    Input: data
+    Output: trainData, used for training your machine learning model
+            testData, used to evaluate the performance of your machine learning model
+            ratio, decide the percentage of training data on the whole dataset.
+    Example:
+            You have a training data with 10000 data record, ratio is 0.7, so you will split the whole dataset and store the first 7000 of them in trainData, and the rest 3000 in testData
+    Instruction:
+            There is no grading script for this function, because different group may select different dataset depending on their course project, but generally you should make sure that you code can divide the dataset correctly, since you may use it for the course project
+    """
+    # your code here
+    pass
+
+def splitDataRandom(data, trainData, testData, ratio):
+    """
+    Input: data
+    Output: trainData, used for training your machine learning model
+            testData, used to evaluate the performance of your machine learning model
+            ratio, decide the percentage of training data on the whole dataset.
+    Example:
+            You have a training data with 10000 data record, ratio is 0.7, so you will split the whole dataset and store 7000 of them in trainData, and 3000 in testData.
+    Instruction:
+            Almost same as splitData, the only difference is this function will randomly shuffle the input data, so you will randomly select data and store it in the trainData
+    """
+    # your code here
+    pass
+
+def main():
+    options = parser.parse_args()
+    mode = options.mode       # first get the mode
+    print("mode is " + mode)
+    """
+    similar to Lab 2, please add your testing code here
+    """
+    # your code here
+    pass
+
+def showHelper():
+    """
+    Similar to Lab 2, please update the showHelper function to show users how to use your code
+    """
+    parser.print_help(sys.stderr)
+    # your code here
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    #------------------------arguments------------------------------#
+    #Shows help to the users                                        #
+    #---------------------------------------------------------------#
+    parser = argparse.ArgumentParser()
+    parser._optionals.title = "Arguments"
+    parser.add_argument('--mode', dest='mode',
+    default = '',    # default empty!
+    help = 'Mode: R for random splitting, and N for the normal splitting')
+
+    """
+    Similar to Lab 2, please update the argument, and add as you need
+    """
+    # your code here
+    if len(sys.argv)<3:
+        showHelper()
+    main()
